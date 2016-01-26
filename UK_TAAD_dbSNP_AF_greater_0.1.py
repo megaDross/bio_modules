422e28a714e81ae36765037bdf044bf3c2ae197b
U-ED\dross11 <dross11@MVM-IGMM-L025.med.ed.ac.uk>
2016-01-26 17:15:35 +0000

    sorted out UK_TAAD_dbSNP_AF_greater_0.1.py thanks to Specter_Terrasbane reddit user. I have split everthing up into smaller functions and used some functional programming techniques

--- UK_TAAD_dbSNP_AF_greater_0.1.py
+++ UK_TAAD_dbSNP_AF_greater_0.1.py
@@ -1,91 +1,90 @@
-import os
-import itertools
-import re
+import re, collections
+# tabix <dbSNP.vcf> <positions>
+# the tabixed file is the UK_vars vcf which was filtered through with dbSNP vcf
 
-op = open("UK_TAAD_Variants_in_dbSNP.vcf") # tabix <dbSNP.vcf> <positions
-out = open("greater_than_0.1_AF.txt","w")
-original = open("var.both.TAAD_UK_Nov2015.filters.vcf","r")
-#
-for i in op:
-    info = i.split("\t")[7]
-    loc = i.split("\t")[0]+":"+i.split("\t")[1]
-    rs = i.split("\t")[2]
-    search_CAF = re.findall(r"CAF=.*;",info)
-    CAF = ','.join(search_CAF).split(",")
-    if len(CAF) > 1:
-            AF = re.sub(";","",CAF[1])
-            RAF = CAF[0]
-            if AF != "." and float(AF) > 0.1:
-                #print loc+"\t"+rs+"\t"+str(AF) 
-                out.write(loc+"\t"+rs+"\t"+str(AF)+"\t"+i.split("\t")[4]+"\n")
-out.close()
-
-out = open("greater_than_0.1_AF.txt","r")
-oot = open("positions_in_TAAD_UK.txt","w")
-positions_in_z = []
-
-
-
-test = open("head.vcf","r")
-
-#for x,z in itertools.product(original,out):
-#    if not x.startswith("#") and not z.startswith("#"):
-#        cells_x = x.split("\t")
-#        pos_x = cells_x[0]+":"+cells_x[1]
-#        alt_x = cells_x[4]
-#        sample_cells_x = cells_x[16:len(cells_x)]
-#        
-#        cells_z = z.split("\t")
-#        pos_z = cells_z[0]
-#        alt_z = cells_z[3]
-#        positions_in_z.append(pos_z+"\t"+alt_z)
-#        
-#        if pos_x+"\t"+alt_x in positions_in_z:
-#            
-#            if "./." in sample_cells_x:
-#                print pos_x + "\t"+ "found in " + \
-#                str(len(sample_cells_x) - sample_cells_x.count("./."))\
-#                + " samples"
-#            else:
-#                print pos_x + "\t" + "nothing"
-#     
-
-dic = {}
-# construct a dictionary where the keys are var pos and the item
-# is the alt nucleotide. All of which is derived from the 
-# greater_than_0.1_AF.txt
-for i in out:
-    if not i.startswith("#"):
-        cells = i.split("\t")
-        pos = cells[0]
-        alt = cells[3]
-        dic[pos]=alt
-
-for (x),(pos,alt) in itertools.product(original,dic.items()):
-    if not x.startswith("#"):
-        cells_x = x.split("\t")
-        pos_x = cells_x[0]+":"+cells_x[1]
-        alt_x = cells_x[4]
-        sample_cells_x = cells_x[16:len(cells_x)]
-        sample_format = filter(lambda b: b != "./.", sample_cells_x)
-        print len(sample_format)
+def extract_relevant_info(file_name):
+    '''extracts the relevant information from the tabixed output file
+    '''
+    dbSNP_UK_Matched_Common = []
+    for row in open(file_name, 'r'):
         
+        info = row.split("\t")[7]
+        pos = row.split("\t")[0]+":"+row.split("\t")[1]
+        rs = row.split("\t")[2]
+        alt = row.split("\t")[4]
+        search_CAF = re.findall(r"CAF=.*;",info)
+        CAF = ','.join(search_CAF).split(",")   # comma seperated allele frequency (ref,alt)
         
-        #if pos in pos_x and int(DP_x) > 50:
-        #    if "./." in sample_cells_x:
-        #        print pos_x + "\t"+ "found in " + \
-        #        str(len(sample_cells_x) - sample_cells_x.count("./."))\
-        #        + " samples"
+        if len(CAF) > 1:
+                AF = re.sub(";","",CAF[1])   # alternative allele frequency 
+                if AF != "." and float(AF) > 0.1: 
+                    dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt)
+    
+    return dbSNP_UK_Matched_Common
+    
+    
+    
+def get_pos_alt_pairs(file_name):
+    """Returns a set of the unique pos/alt pairs from all rows in the given file"""
+    
+    pairs = set()     # eliminate replicates 
+    
+    for row in file_name:
+            pos_alt = row.split('\t')[0:4:3]   #extract 0 and 3 indices
+            pos_alt_pair = tuple(map(str.strip,pos_alt))  # remove newlines from the list
+            pairs.add(pos_alt_pair)   #append these pairs into empty set
+    return pairs
 
-        if pos in pos_x:
-            if "./." in sample_cells_x:
-                print pos_x + "\t"+ "found in " + \
-                str(len(sample_cells_x) - sample_cells_x.count("./."))\
-                + " samples"
-        
-#print dic.get("18:48584855")
 
-# Look for samples that are of a certain depth only, look at genotype field 
-# and VCF Specification list
-#   - so only pull samples from the original if > 50 RD & Alt Genotype...
-        
+def get_matching_rows(file_name, valid_pos_alt_pairs):
+    """Returns a collection of matching rows.
+
+    Compares the pos/alt from each row in the given file against a master pos/alt list.
+    If a match is found, the row's fields are further validated to check if it
+    is a row of interest.  If so, it gets added to the returned collection.
+    """
+
+    matching_rows = []
+   
+    with open(file_name, 'r') as input_file:
+        for row in input_file:
+            if not row.startswith('#'):    # removes headers
+                
+                cells = row.split('\t')
+                pos = '{0}:{1}'.format(cells[0], cells[1])
+                alt = cells[4]
+                pos_alt_pair = (pos, alt)
+                
+                if pos_alt_pair in valid_pos_alt_pairs:
+                    cell_values = [cell.split(':') for cell in cells[16:]]    # this creates a list of lists containing sample info fields
+                    remove_empty_cells = filter(lambda a: "./." not in a, cell_values)
+                    
+                    for cleaned_cell_values in remove_empty_cells:
+                        if len(cleaned_cell_values) == 5 and "1/1" in cleaned_cell_values[0] and int(cleaned_cell_values[2]) > 49:    # must have a 1/1 genotype and alternative AD > 49
+                            matching_rows.append(pos_alt_pair)  # append the (pos,alt) associated with each individually filtered sample info field
+
+    matching_rows_count = collections.Counter(matching_rows)   # count no. of duplicates in list; no. of variants common in our cohort
+    return matching_rows_count#[('20:45354291', 'A')]
+
+
+
+          
+def process_files(file_name_1, file_name_2):
+    """Returns rows from the second file that match both pos/alt and a valid cell value"""
+    
+    dbSNP_UK_Matched_Common = extract_relevant_info(file_name_1)
+    valid_pos_alt_pairs = get_pos_alt_pairs(dbSNP_UK_Matched_Common)
+    matching_rows_count = get_matching_rows(file_name_2, valid_pos_alt_pairs)
+    
+    most_common_variants = []    
+    
+    for pos,alt in matching_rows_count.most_common():
+        most_common_variants.append(str(pos[0]) +","+str(pos[1])+"," + str(alt))
+
+    return most_common_variants
+    
+    
+    
+    
+    
+print process_files("UK_TAAD_Variants_in_dbSNP.vcf","test.txt")
