494fe44f5b1ac5527fcbf5bdb7a490605a98f6b7
U-ED\dross11 <dross11@MVM-IGMM-L025.med.ed.ac.uk>
2016-02-02 18:39:55 +0000

    Added a new function which allows one to get the index numbers for those samples contained in a vcf header, from an inputted sample list

--- UK_TAAD_dbSNP_AF_greater_0.1.py
+++ UK_TAAD_dbSNP_AF_greater_0.1.py
@@ -11,14 +11,24 @@ def extract_relevant_info(file_name):
         info = row.split("\t")[7]
         pos = row.split("\t")[0]+":"+row.split("\t")[1]
         rs = row.split("\t")[2]
-        alt = row.split("\t")[4]
+        alt = row.split("\t")[4] 
+        
         search_CAF = re.findall(r"CAF=.*;",info)
         CAF = ','.join(search_CAF).split(",")   # comma seperated allele frequency (ref,alt)
         
         if len(CAF) > 1:
                 AF = re.sub(";","",CAF[1])   # alternative allele frequency 
-                if AF != "." and float(AF) > 0.1: 
-                    dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt)
+                if AF != "." and float(AF) > 0.29: 
+                    dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt.split(",")[0])
+                    
+                    ## THERE MUST BE A BETTER WAY TO DO THE MULTIPLE IF STATEMENTS BELOW
+                    
+                    if len(alt.split(",")) > 1:   # if the alt variable has more than one value, use the second
+                        dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt.split(",")[1])
+                    if len(alt.split(",")) > 2:   #if the alt variable has more than one value, use the third
+                        dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt.split(",")[2])
+                    if len(alt.split(",")) > 3:   #if the alt variable has more than one value, use the fourth
+                        dbSNP_UK_Matched_Common.append(pos+"\t"+rs+"\t"+str(AF)+"\t"+alt.split(",")[3])
     
     return dbSNP_UK_Matched_Common
     
@@ -33,8 +43,9 @@ def get_pos_alt_pairs(file_name):
             pos_alt = row.split('\t')[0:4:3]   #extract 0 and 3 indices
             pos_alt_pair = tuple(map(str.strip,pos_alt))  # remove newlines from the list
             pairs.add(pos_alt_pair)   #append these pairs into empty set
+    
     return pairs
-
+    
 
 def get_matching_rows(file_name, valid_pos_alt_pairs):
     """Returns a collection of matching rows.
@@ -60,8 +71,42 @@ def get_matching_rows(file_name, valid_pos_alt_pairs):
                     remove_empty_cells = filter(lambda a: "./." not in a, cell_values)
                     
                     for cleaned_cell_values in remove_empty_cells:
-                        if len(cleaned_cell_values) == 5 and "1/1" in cleaned_cell_values[0] and int(cleaned_cell_values[2]) > 49:    # must have a 1/1 genotype and alternative AD > 49
-                            matching_rows.append(pos_alt_pair)  # append the (pos,alt) associated with each individually filtered sample info field
+                        if len(cleaned_cell_values) == 5:
+                            
+                            DP = int(cleaned_cell_values[2])
+                            GT = cleaned_cell_values[0]
+                            AD_ref = int(cleaned_cell_values[1].split(",")[0])
+                            AD_alt = int(cleaned_cell_values[1].split(",")[1])
+                            
+                            GQ = int(cleaned_cell_values[3])
+                            
+                            if (GT == "1/1" or GT == "2/2" or GT == "3/3" or GT == "4/4") and DP > 49 and GQ >29:    
+                                matching_rows.append(pos_alt_pair)  # append the (pos,alt) associated with each individually filtered sample info field
+                                print cleaned_cell_values
+                                
+                            if (GT == "0/1" or GT == "1/0") and AD_alt > 49 and GQ > 29:
+                                matching_rows.append(pos_alt_pair)
+                                print cleaned_cell_values
+                                
+                            if (GT == "2/0" or GT =="0/2"):
+                                AD_second_alt = int(cleaned_cell_values[1].split(",")[2])
+                                if AD_second_alt > 49 and GQ > 29:
+                                    matching_rows.append(pos_alt_pair)
+                            
+                            if (GT == "3/0" or GT == "0/3"):
+                                AD_third_alt = int(cleaned_cell_values[1].split(",")[3])
+                                if AD_third_alt > 49 and GQ > 29:
+                                    matching_rows.append(pos_alt_pair)
+                            
+                            if (GT == "4/0" or GT == "0/4"):
+                                AD_fourth_alt = int(cleaned_cell_values[1].split(",")[4])
+                                if AD_fourth_alt > 49 and GQ > 29:
+                                    matching_rows.append(pos_alt_pair)
+                            
+                                
+                            
+                                
+                            
 
     matching_rows_count = collections.Counter(matching_rows)   # count no. of duplicates in list; no. of variants common in our cohort
     return matching_rows_count#[('20:45354291', 'A')]
@@ -71,6 +116,9 @@ def get_matching_rows(file_name, valid_pos_alt_pairs):
           
 def process_files(file_name_1, file_name_2):
     """Returns rows from the second file that match both pos/alt and a valid cell value"""
+    output = open("hopeful_output.txt","w")
+    output.write("variant_pos"+"\t"+"ALT"+"\t"+"sample_number"+"\n")
+    
     
     dbSNP_UK_Matched_Common = extract_relevant_info(file_name_1)
     valid_pos_alt_pairs = get_pos_alt_pairs(dbSNP_UK_Matched_Common)
@@ -80,11 +128,13 @@ def process_files(file_name_1, file_name_2):
     
     for pos,alt in matching_rows_count.most_common():
         most_common_variants.append(str(pos[0]) +","+str(pos[1])+"," + str(alt))
-
+        output.write(str(pos[0]) +"\t"+str(pos[1])+"\t" + str(alt)+"\n")
+    
+    output.close()
     return most_common_variants
     
     
     
     
     
-print process_files("UK_TAAD_Variants_in_dbSNP.vcf","test.txt")
+print process_files("UK_TAAD_Variants_in_dbSNP.vcf","var.both.TAAD_UK_Nov2015.filters.vcf")
