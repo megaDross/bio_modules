3598e7d502c566c802e81f80f3c6d3b80b174711
david <dross78375@gmail.com>
2016-02-10 22:32:21 +0000

    the updated exon_fun is basically completed. I have split it into functions. Now I just need to put it all together. the old version has been deleted

--- exon_fun.py
+++ exon_fun.py
@@ -1,71 +1,94 @@
-import urllib2
-import re
+import requests, json
 
-### WHY ARE YOU DOING THIS ###
-# having looked at my validation database for variants AB>0.3 and not validating,
-# I wonder whether I can see whether they cluster around the same exons? this would
-# indicate whether some genomic areas are harder to sequence than others.
 
-### POTENTIAL PROBLEMS ###
-# the variable 'pos' is essentially the coordinates for where the variant occurs along
-# with a dash and number reresenting 101bp upstream. This extra 200bp is required to 
-# produce a range, as a range is all that can be plugged into the REST API.
-# This could be problematic later on!!!!
 
-transcript = "ENST00000316623.5:c.8504dupC"
-pos = "15:48703298-48703498"
-#pos = "7:140424943-140624564"
+transcript_and_hgvs = "ENST00000316623.5:c.8504dupC"
+transcript = transcript_and_hgvs.split(":")[0][:-2]
+pos = "15:48703298"
+
 name = "LX14-AI-UKB2"
 
-
-transcript2 = transcript.split(":")[0]
-
-# use the Ensembl code to output transcript page and isolate total number of exons
-url =urllib2.urlopen("http://grch37.ensembl.org/Homo_sapiens/Transcript/Exons?t="+transcript2)
-search = url.read()
-split_url = re.search(r"transcript has.* exons",search).group().split(">")
-total_exons = split_url[len(split_url)-1].split(" ")[0]
-
-
-# prints the end position of the exon from the variant position range given
-# planning on using as a search term for the exons_list variable output
-exon_url = urllib2.urlopen("http://grch37.rest.ensembl.org/overlap/region/human/"+pos+\
-"?feature=exon;content-type=application/json").read()
-end_pos_exon = re.search(r"end.*[0-9],",exon_url).group().split(",")[0].split(":")[1]
-
-
-# prints all exons associated with Ensembl Code, and split them into seperate elements
-exons_list = urllib2.urlopen("http://grch37.rest.ensembl.org/overlap/id/"+\
-transcript2[:len(transcript2)-2]+"?feature=exon;content-type=application/json;expand=1").read()
-seperated_exons_list = re.split(r'[{}]',exons_list)
-
-results = []
-
-# within the seperated exons list, try and find an entry which contains the nucleotide 
-# end position and the ENSEMBL transcript ID and place this into an empty list. The empty
-# list converts the nested lists into one single list, where every nested list becomes 
-# an element.
-for x in range(1,len(seperated_exons_list)):
-    split_all_exons = re.split(r'[{}]',exons_list)[x] # = seperated_exons_list[x]
-    search_all_exons = re.findall(transcript2[:len(transcript2)-2]+".*"+end_pos_exon+".*",split_all_exons)
-    if search_all_exons is not None:
-        results.append(search_all_exons)
-
-# this gets rid of the empty lists ([]) within the list variable named 'empty'
-matches = [z for z in results if z]
-print matches
-# This extracts the exon ID from the matched exon 
-ID = str(matches[0]).split(",")[6]
-almost = ID.split(":")[1]
-exon_ID = re.sub("\"","",almost)
-
-# This extracts the exon number from the matched exon
-exon_number = str(matches[0]).split(",")[9].split(":")[1]
-
-
-print exon_ID +":"+"\t"+exon_number+"/"+total_exons
+# need to put it all together now
 
 
 
-#print exons_list
-#print re.search(end_pos_exon+".*}",exons_list).group()
+def request_ensembl(transcript):
+    ''' Retrieve all expanded exon information associated with the transcipt ID
+        from the Ensembl REST API.
+        
+        The resulting list of dicts will be stored as a global variable
+    '''
+    global all_exon_data
+    
+    url = "http://grch37.rest.ensembl.org/overlap/id/"
+    ext = "?feature=exon;content-type=application/json;expand=1"
+    req = requests.get(url+transcript+ext)
+    req.raise_for_status()
+    all_exon_data = json.loads(req.text)
+    
+    
+    
+    
+def get_exon_information():
+    '''Get every Exon_ID and its start and stop position which reside within 
+       the inputted transcript ID 
+       
+       A conditional is used to extract only the dictionarys containing the 
+       transcript ID. The start, stop positions and exon_id for each matching 
+       dictionary is then stored in a tuple named exon_region and returned. 
+    '''
+    exon_region = set()
+    for exon_dicts in all_exon_data:
+        if exon_dicts.get("Parent") == transcript:
+            start = exon_dicts.get("start")
+            end = exon_dicts.get("end")
+            exon = exon_dicts.get("exon_id")
+            exon_start_end = (exon,start,end)
+            exon_region.add(exon_start_end)
+            
+    return exon_region
+    
+    
+
+def get_exon_id(exon_region):  
+    ''' Get the exon id for which the variant position is within.
+    
+        Use the exon_region tuple to iterate between the start and stop 
+        positions of each exon within the transcript until one number 
+        matches the variant position number, then return the exon_id 
+        associated with that position.
+    '''  
+    exon_id = []      
+    for exons in exon_region:
+        for x in range(exons[1],exons[2]):
+            if x == int(pos.split(":")[1]):
+                exon_id.append(exons[0])
+    
+    return exon_id
+    
+
+def exon_number(exon_id):
+    ''' Returns the exon number in which the variant is within.
+    
+        use the exon_id to retrieve the matching dictionary and extract
+        the rank (exon number) from that dictionary.
+    '''
+    for y in all_exon_data:
+        if y.get("exon_id") == exon_id[0]:
+            exon_number = y.get("rank")
+    return exon_number
+    
+    
+def total_exons():
+    ''' Get the last exon number in the transcript
+    
+        List comprehension which extracts all exon numbers found in all the 
+        dictionarys with the transcript id. Extract and return the last element
+        as the total_exons in the generated list.
+    '''
+    all_exon_ranks = [i.get("rank") for i in all_exon_data if i.get("Parent") == transcript]
+    total_exons = all_exon_ranks[-1]
+    return total_exons
+
+
+
