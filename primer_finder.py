4c7c7dbcf2751ec9f774d22610be7151bf182d37
david <dross78375@gmail.com>
2016-03-10 21:50:54 +0000

    primer_finder has been completely re-written. A string can now be applied to the commandline as well as a file. The following is still needing addressed:
    	- if no primer match then return "cant find primer pair for var_name", proving
    	  to be difficult as if answer not None, if answer != "" etc isnt working
    	  very frustrating!!!!!!!!!
    
    	- exception handeling
    
    	- clean up the first function as some code has been duplicated, if output_file
    	  is not None condition.
    
    	- restructure the file so the TAAD primer database is used as default if no other
    	  primer_database argument is given

--- primer_finder.py
+++ primer_finder.py
@@ -1,117 +1,106 @@
-#!/usr/bin/python
-import re
-import csv
-import itertools
+import re, os.path
 
 
-def primer_finder(input_file, primer_database, output_file, delimiters=None):
-	'''Takes an input file consisting of variant positions (input_file) and finds
-	primers within a primer_database which cover said variant. Allowing one to find
-	possible primers and outputs them into a new_file.
+#exception handeling and use string as input
 
-	Input file should be in he following format:
-		variant_name	Variant_position
-	primer_database format:
-		Primer_name	F-primer	R-primer	Product_size	Region
-	
-	The delimiters argument is defaulted to tab
-	'''
-	####################### PREPERATION ############################
-	
-	# change dir to where the txt/csv files reside and open the files that contain primer
-	# information and one which conatins new variants and variant position. use csv module
-	# to split columns by tab and allow one to manipulate each column. open two empty
-	# lists, one empty dictionaries and writable empty file.
-	
-	if delimiters is None:
-	   delimiters = "\t"
+def primer_finder(input_file,primer_database,output_file=None,delimiters="\t"):
+    ''' Takes variant postion(s) and matches it with a primer in the primer 
+        database. 
+       
+        input_file should be in the following format (tab deliminated):
+            variant_name    variant_position
+        
+        primer_database should be in the following format (tab deliminated):
+            primer name    F-primer    R-primer    product size    genomic region 
+        
+        output_file is optional and delimiters is tab by default
+    '''
+    primer_file = open(primer_database)
+    all_primer_pos = get_all_primer_pos(primer_file)
+    
+    if os.path.isfile(input_file) is False:
+        var_pos = re.sub(r'[^0-9:]','',input_file)
+        matched_primers = match(var_pos,all_primer_pos)
+        return matched_primers
+            
+    if output_file is not None:
+        output_file = open(output_file,"w")
+        output_file.write("Variant_Name"+"\t"+"Primer_Name")
+        
+        var_file = open(input_file)
+        for var in var_file:
+            var_name = var.split("\t")[0]
+            var_pos = re.sub(r'[^0-9:]','',var.split("\t")[1])
+            matched_primers = match(var_pos,all_primer_pos,var_name)
+            
+            print matched_primers
+            output_file.write("".join((matched_primers,"\n")))
+        output_file.close()
+    
+    else:    
+        var_file = open(input_file)
+        for var in var_file:
+            var_name = var.split("\t")[0]
+            var_pos = re.sub(r'[^0-9:]','',var.split("\t")[1])
+            matched_primers = match(var_pos,all_primer_pos,var_name)
+            print matched_primers
+            
+    
+        
+        
+def get_all_primer_pos(primer_file):
+    ''' Generate evey genomic position within each primer and concatenate it with
+        the primer name and pack within a list; each list contains every position 
+        within a primer pair. Append every list to an empty list and unpack the list of
+        lists with a nested list comprehension.
+    '''
+    header = primer_file.next()
+    all_primer_pos = []
+    
+    for i in primer_file:
+        
+        i = i.split("\t")
+        primer_name = i[0]
+        primer_range = re.split(r'[:-]',i[4])   # split into three components
+        chrom = re.sub(r'[^0-9]','',primer_range[0])  # remove any non-integers
+        start = int(primer_range[1])
+        stop = int(primer_range[2])
+        
+        all_pos_in_primer = [ " ".join((primer_name,chrom+":"+str(i))) 
+                            for i in range(start,stop)]
+        all_primer_pos.append(all_pos_in_primer)
+        
+    all_primers_pos_unpacked = [x for i in all_primer_pos for x in i]   # unpack list of lists
+    return all_primers_pos_unpacked
+        
+        
+        
+def match(var_pos,primer_info,var_name=None):
+    ''' Match a given variant position against every genomic position covered
+        in all the primers in the primer database.
+    '''
+    
+    for i in primer_info:
+        
+        if var_name is None:   # used if a string given as input instead of a file
+            var_name = var_pos
+        
+        primer_name = i.split(" ")[0]
+        primer_pos = i.split(" ")[1]
+        
+        
+        ## if no match then print cant find matching primer
+        ## proving to be challenging, not working as expected
+        ## if not Non, if != "" if not "" etc. not working, just returns None, regardless
+        
+        if var_pos == primer_pos:
+            answer = ",".join((var_name,primer_name))
+            return answer
+                 
+        
+    
+    
+    
 
-	primer_file = open(primer_database,"r")
-	variant_file = open(input_file,"r")
-	
-	product_file =open(output_file,"w")
-	
-	primers_pos = csv.reader(primer_file, delimiter="\t")
-	variant_pos = csv.reader(variant_file, delimiter="\t")
-	
-	var_area = {}
-	primer_area = {}
-	primerz = []
-	product = []
-	
-	
-	#################### PRIMER POSITION LIST #######################
-	
-	# select fifth column of primer info file (region) and only select if begins with "chr".
-	# split into three variables chromosome, primer start and end site. create a new loop
-	# which produces a int for every number between start and stop site for each primer pair
-	# with the the chromosome number prefixed to each number and append them to the empty
-	# primerz list, to create a list element for every genome position in which a primer pair
-	# covers. 
-	for region in primers_pos:
-    	   #prim = region[0]
-    	   #reg = region[4]
-    	   #primer_area[prim] = reg
-    	   if re.search(r'chr', region[4]):
-	       chromosome = re.split(r'[:-]',region[4])[0]
-	       start = int(re.split(r'[:-]',region[4])[1])
-	       stop = int(re.split(r'[:-]',region[4])[2])
-	       for numbers in range(start,stop):
-                   	primer_area = chromosome + ":" + str(numbers)
-                   	primerz.append(region[0] + " " + primer_area)     
-	
-            	
-	#################### VARIANT DICTIONARY ##############################
-	
-	# prodce a dictionary that contains all varinat names as keys and the equivalent 
-	# ivar poisition as items and store them within the empty dictionary.
-	for var in variant_pos:
-           varz = var[1]
-    	   name = var[0]
-    	   var_area[name] = varz
-    	
-    	
-	
-    	
-	
-	#################### PRIMER FINDER ####################################
-	
-	# loop both the newly created primerz list and the newly created dictionary, with the keys
-	# placed in the variable var_name and the items in the variable var_pos. If the var_pos is found
-	# in the list of primers then append the variants name along with the primer pair name that
-	# can be used to produce an amplicon for said variant name.
-	# 
-	# itertools allows one to produce loops with multiple variables. itertools.product is like zip
-	# expect the two loops don't have to be the same length and unlike itertools.izip it doesn't
-	# produce NoneTypes for extra entries in shortest lengthed loop. Instead it loops the shortest length
-	# loop to the same size as the longest length loop (print var_name in the loop to understand)
-	#
-	# produce a loop that prints product list minus duplicate entries (set) and also writes them to the
-	# empty file.
-	for (primer_pos),(var_name,var_pos) in itertools.product(primerz,var_area.items()):
-    	   if re.search(var_pos,primer_pos):
-	       matched_primer_pos = primer_pos.split()[0]
-	       product.append("variant: "+var_name +", "+ " primers: " + str(matched_primer_pos))
-	
-	
-	
-	for match in list(set(product)):
-    	   print match
-    	#matche_primer = re.split(":",match)[2][1:]
-    	   product_file.write(match+"\n")
-        product_file.close()
-	
-	
-	#for that in product:
- 	#   print re.split(":",that)[2][1:]
-	
-	
-	
-	# Below shows how one can find matching entries in two lists using set().intersection()
-	a = ["red","yellow","blue"]
-	b = ["yellow","green","purple","red"]
-	
-	intersect = list(set(a).intersection(b))
-	print "\n" + str(intersect)
-	
-	
+print primer_finder("2:189859300","TAAD_Primer_Validation_Database.txt")
+#print primer_finder("in.txt","TAAD_Primer_Validation_Database.txt")
