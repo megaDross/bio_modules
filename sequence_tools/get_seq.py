fd714fc11a27e30be5f1423097db7c95cc53e410
U-ED\dross11 <dross11@MVM-IGMM-L025.med.ed.ac.uk>
2016-03-18 16:31:16 +0000

    genomic range can now have a dash instead of a comma for deliminating rangeinstead of going straight to error. changed name of get_seq_OOP.py to get_seq.py

--- sequence_tools/get_seq.py
+++ sequence_tools/get_seq.py
@@ -1,64 +1,41 @@
 from __future__ import division
 import os, sys,re, urllib2, click
 
+#### error handeling of "variant not recognised by UCSC" may need to be rethought
 
-#### exception handeling and commenting still needed
-#### CREATE ANOTHER FUNCTION THAT HANDLES ALL ARGUMENT ERRORS
+@click.command('get_seq')
+@click.argument('input_file',nargs=1)
+@click.option('--output_file',default="get_seq_output.txt", help='default: get_seq_output.txt')
+@click.option('--upstream', default=20, help="number of bases to get upstream, default: 20") # default to an int makes option accept int only
+@click.option('--downstream',default=20, help="number of bases to get downstream, default: 20")
+@click.option('--hg_version',default="hg19", help="human geome version. default: hg19")
+@click.option('--delimiters',default="\t", help="file delimiter. default: tab")
+@click.option('--dash/--no_dash',default='n', help="dashes flanking the variant position base. default: --no_dash") # the slash in the option makes it a boolean
 
-
-@click.command()
-@click.option('--input_file')
-@click.option('--output_file',default="get_seq_output.txt")
-@click.option('--upstream', default=20)
-@click.option('--downstream',default=20)
-@click.option('--hg_version',default="hg19")
-@click.option('--delimiters',default="\t")
-@click.option('--dash',default="Y")
-
-def get_seq(input_file, 
-            output_file="get_seq_output.txt",
-            upstream=20, 
-            downstream=20, 
-            hg_version="hg19", 
-            delimiters="\t",
-            dash="Y"
-            ):
+def get_seq(input_file, output_file="get_seq_output.txt", upstream=20, downstream=20, hg_version="hg19", delimiters="\t",dash="n"):
                      
-        ''' Produce a sequence using the UCSC DAS server from a variant postion and 
-            number of bases upstream and downstream from said position.
-        
-            Arguments
-            --------------------------------------------------------------------
-            input_file -- a variant name and variant position tab deliminated file
-            
-            output_file -- created only if the input is a file opposed to a string
-                           if no name is sepcified for output_file, then it is 
-                           automatically named get_seq_output.txt
-                           
-            upstream -- number of bases to get upstream from the given variant position
-            
-            downstream -- number of bases to get downstream from the given varant position
-            
-            hg_version -- the version of the human genome to scrape the sequence from
-                          i.e. hg19, hg38
-                          
-            dash -- choose whether to include dashes flanking the variant positions base
-                    or not i.e. N = no, Y = yes. defaulted to Y
-                          
-            Returns
-            --------------------------------------------------------------------
-            sequence -- sequence associated with the given seq_range AKA genomic range
-                        with a dash flanking the base associated with the variant position
-                        
-            Examples
-            --------------------------------------------------------------------
-            
-            
+        ''' 
+        Produce a sequence using the UCSC DAS server from an inputted variant 
+	postion and defined number of bases upstream and downstream from said 
+	position.
+         \b\n
+        A file or string can be used as input.
+	STRING: either a variant position or a genomic range deliminated by a comma
+	FILE: deliminated file with the variant name and the variant position
+         \b\n
+        Example:\b\n
+           python get_seq_OOP.py chr1:169314424 --dash --hg_version hg38\n
+           python get_seq_OOP.py chr1:169314424,169314600\n
+           python get_seq_OOP.py input.txt --output_file output.txt --dash\n
         '''
             
-        # handle any errors in the arguments    
-        handle_argument_exception(input_file,output_file,upstream,downstream,hg_version,delimiters,dash)
-        
+        # exit if any errors is found in the arguments for STRING input   
+        process = Processing(input_file,output_file,upstream,downstream,hg_version,delimiters,dash)
+        error_check =  process.handle_argument_exception()
+        if error_check is not None:
+            print error_check
+            sys.exit(0)
+            
         # if input_file is a file
         if os.path.isfile(input_file) is True:
             output=open(output_file,"w")
@@ -68,133 +45,117 @@ def get_seq(input_file,
                 seq_name = changes[0]
                 var_pos = changes[1]
                 
-                seq_range = create_region(var_pos,upstream,downstream)
+                # check each individual line of the FILE for errors
+                process = Processing(var_pos,output_file,upstream,downstream,hg_version,delimiters,dash)
+                error_check = process.handle_argument_exception()
+                
+                # if error is returned
+                if error_check is not None:
+                    print error_check
+                    continue
+                
+                # if create_region is unsuccessful then move to next element
+                seq_range = process.create_region(var_pos)
                 if seq_range is None:
                     continue
-                    
-                answer = get_region_info(seq_range,upstream,downstream,hg_version,dash)
+                
+                # if get_region_info is unsuccessful then move to next element
+                answer = process.get_region_info(seq_range)
                 if answer is None:
                     continue
-                    
-                sequence = "\t".join((seq_name,seq_range,answer))
+                
+                # concatenate the name and outputs from Class
+                sequence = " ".join((">",seq_name,var_pos,seq_range,"\n",answer,"\n"))
                 print sequence
-                output.write(sequence+"\n")
+                output.write(sequence)
                 
             output.close()
         
+        
         # if input_file is a genomic range
-        if os.path.isfile(input_file) is False and re.search(r",",input_file):
-            dash = "N"
-            var_pos = re.sub(r'[^0-9:,]','',input_file) 
-            sequence = get_region_info(var_pos,upstream,downstream,hg_version,dash)
+        if os.path.isfile(input_file) is False and re.search(r"[,-]",input_file):
+            
+            # remove unwanted characters from the range
+            seq_range = re.sub(r'[^0-9:,-]','',input_file) 
+            if "-" in seq_range:
+                seq_range = seq_range.replace("-",",")
+            sequence = process.get_region_info(seq_range)
             click.echo(sequence)
             return sequence
+        
         
         # if input_file is a genomic position
         if os.path.isfile(input_file) is False:
+            
+            # remove unwanted characters from var position
             var_pos = re.sub(r'[^0-9:]','',input_file)
-            seq_range = create_region(var_pos,upstream,downstream)
-            sequence = get_region_info(seq_range,upstream,downstream,hg_version,dash)
+            seq_range = process.create_region(var_pos)
+            sequence = process.get_region_info(seq_range)
             click.echo(sequence)
             return sequence
-        
-        
 
-def handle_argument_exception(  input_file, 
-                                output_file="get_seq_output.txt",
-                                upstream=20, 
-                                downstream=20, 
-                                hg_version="hg19", 
-                                delimiters="\t",
-                                dash="Y"
-                                ):
-                                
-            if hg_version not in ["hg16","hg17","hg18","hg19","hg38"]:
-                raise click.ClickException("Human genome version "+hg_version+" not recognised")
+
+
+class Processing():
+
+    def __init__(self,input_file,output_file,
+                 upstream, downstream, hg_version, 
+                 delimiters,dash):
+        
+            self.input_file = input_file
+            self.output_file = output_file
+            self.upstream = upstream
+            self.downstream = downstream
+            self.hg_version = hg_version
+            self.delimiters = delimiters
+            self.dash = dash
+        
+        
+    def handle_argument_exception(self):
+            ''' Stores custom exceptions
+            '''        
+            
+            if self.hg_version not in ["hg16","hg17","hg18","hg19","hg38"]:
+                raise click.ClickException("Human genome version "+self.hg_version+" not recognised")
                 sys.exit(0)
                 
-            if not int(upstream) or not int(downstream):
-                raise click.ClickException("Ensure the string given for upstream "+
-                "and/or downstream are integers:\n\tupstream: "+str(upstream)+
-                "\t downstream: "+str(downstream))
-                sys.exit(0)
-                
-            if os.path.isfile(input_file) is False:
-                if input_file.count(",") > 1:
-                    raise click.ClickException("too many commas in "+input_file)
-                
-                if input_file.count(":") < 1 or input_file.count(":") >1:
-                    raise click.ClickException("A single colon is required to seperate"+ 
+            if os.path.isfile(self.input_file) is False:
+                if self.input_file.count(",") > 1:
+                    return("too many commas in "+self.input_file)
+                    
+                    
+                if self.input_file.count(":") < 1 or self.input_file.count(":") >1:
+                    return("A single colon is required to seperate "+ 
                     "the chromosome and position numbers in the variant position: "+
-                    input_file)
+                    self.input_file)
+                                             
                 
-            if dash not in ["Y","N","y","n"]:
-                raise click.ClickException("Only Y or N can be used with the --dash argument")
-                sys.exit(0)
-
-def create_region(var_pos,upstream=20, downstream=20):
+    def create_region(self,var_pos):
             ''' use the variant position given, add and subtract the 
-                numbers given in upstream and downstream
-                to return a genomic range.
-                
-                var_pos -- variant position
-                
-                upstream -- number of bases to get upstream from the given variant position
-            
-                downstream -- number of bases to get downstream from the give n variant position
-            
+                numbers given in upstream and downstream respectively
+                from the given variant position to return a genomic range.
             '''
             
             # create a genomic range
-            try:
-                nospace = var_pos.replace(" ","")
-                chrom = nospace.split(":")[0]
-                pos = nospace.split(":")[1]
-                start_pos = int(pos) - upstream
-                end_pos = int(pos) + downstream
-                seq_range = chrom+":"+str(start_pos)+","+str(end_pos)
-                return seq_range
+            nospace = var_pos.replace(" ","")
+            chrom = nospace.split(":")[0]
+            pos = nospace.split(":")[1]
+            start_pos = int(pos) - self.upstream
+            end_pos = int(pos) + self.downstream
+            seq_range = chrom+":"+str(start_pos)+","+str(end_pos)
+            return seq_range
                 
-            except IndexError:
-                print("A colon is required to seperate the chromosome and position"
-                " numbers in the variant position: "+var_pos)
-            except TypeError:
-                print("Ensure the string given for upstream and/or downstream are integers:\n"+
-                "\tupstream: "+str(upstream)+"\t downstream: "+str(downstream))
-                sys.exit(0)
-    
-        
-def get_region_info(seq_range, upstream,downstream,hg_version,dash):
+                
+    def get_region_info(self, seq_range):
         ''' From a genomic range and human genome version, use UCSC DAS server
             to retrieve the sequence found in the given genomic range.
-            
-            Arguments
-            -------------------------------------------------------------------
-            seq_range -- a genomic range i.e. chr1:2400000,2408002
-            
-            upstream -- number of bases to get upstream from the given variant position
-            
-            downstream -- number of bases to get downstream from the give n variant position
-            
-            hg_version -- version of the human genome i.e. hg19, hg38
-            
-            dash -- choose whether to include dashes flanking the variant positions base
-                    or not i.e. N, Y. defaulted to Y
-            
-            Returns
-            --------------------------------------------------------------------
-            answer -- sequence associated with the given seq_range AKA genomic range
-            
         '''
         # scrape for the sequence associated with the seq_range AKA genomic region 
-        test = urllib2.urlopen("http://genome.ucsc.edu/cgi-bin/das/"+hg_version+"/dna?segment="+seq_range)
+        test = urllib2.urlopen("http://genome.ucsc.edu/cgi-bin/das/"+self.hg_version+"/dna?segment="+seq_range)
         search = test.read()
         refined_search = re.findall(r"[tacg{5}].*",search)
         confirm_error = re.findall(r"nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",search)
         
-        # BELOW IS REDUNDANT AS IT IS HANDLED BELOW AND IN THE TOP FUNCTION
-        if confirm_error:
-            return "Variant not recognised by UCSC"
             
         # filters for elements which only contain nucleotides and concatenate
         seqs = [s for s in refined_search if not s.strip("tacg")] 
@@ -203,10 +164,10 @@ def get_region_info(seq_range, upstream,downstream,hg_version,dash):
         
         # flank the base associated with the variant position with dashes
         try:
-            if dash == "Y" or dash == "y":
-                downstream = seq[:upstream]
-                var = seq[upstream]
-                upstream = seq[upstream+1:len(seq)]
+            if self.dash:
+                downstream = seq[:self.upstream]
+                var = seq[self.upstream]
+                upstream = seq[self.upstream+1:len(seq)]
                 answer = "".join((downstream,"-",var,"-",upstream))
                 return answer
         
@@ -214,13 +175,9 @@ def get_region_info(seq_range, upstream,downstream,hg_version,dash):
             return "Variant not recognised by UCSC"
         
         # return sequence without dashes
-        if dash == "N" or dash == "n":
+        if not self.dash:
             return seq
-        
-        # error message
-        else:
-            print "only Y or N can be given for --dash option"
-        
+            
                 
 if __name__ == '__main__':
     get_seq()         
