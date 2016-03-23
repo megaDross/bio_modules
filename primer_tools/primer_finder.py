52adf8e54b40348d242e5ace21ad6c25cb3cb323
david <dross78375@gmail.com>
2016-03-23 20:09:15 +0000

    primer_finder and unknown_primer have been updated to be more command line friendly.
    exception handeling in primer_finder needs to be imporved.

--- primer_tools/primer_finder.py
+++ primer_tools/primer_finder.py
@@ -1,79 +1,39 @@
-import re, os.path, sys
-import useful 
-import click
+import re, os.path, sys, useful, click
 
 # gives the dir/PATH of this program, will be used to find the defaulted primer_database
+# which are hg19 primers only.
 file_path = useful.cwd_file_path(__file__) 
 
-
-# allows one to use this as a command line tool
-# renders the program unuseable in a python terminal
-@click.command() #decorator converts the function into a command
-
-@click.option('--input_file', 
-              help="input; should be a variant position string or a file")
-              
-@click.option('--output_file',
-              default="matching_primers_output.txt",
-              help="output; defaulted as matching_primers_output.txt")
-              
-@click.option('--primer_database',
-              default=file_path+"TAAD_Primer_Validation_Database.txt",
-              help="defaulted to TAAD primer DB")
-              
+@click.command() 
+@click.argument('input_file',nargs=1)
+@click.option('--output_file',default=None,help="output; defaulted as matching_primers_output.txt")
+@click.option('--primer_database',default=file_path+"TAAD_Primer_Validation_Database.txt",help="defaulted to TAAD primer DB")  
 @click.option('--delimiters', default="\t",help="defaulted to tab")
 
-
 def matching_primer(input_file,
-                    output_file = "matching_primers_output.txt",
+                    output_file = None,
                     primer_database = file_path+"TAAD_Primer_Validation_Database.txt",
                     delimiters = "\t"):
                     
     ''' Takes variant postion(s) as input and matches it with an appropriate primer
         pair in a given primer database. 
-        
-        Arguments
-        -------------------------------------------------------------------------
-        input_file -- should be a variant position string or a file in the following 
-                      format (tab deliminated):
-                            variant_name    variant_position
-        
-        primer_database -- should be in the following format (tab deliminated):
-                              primer name    F-primer    R-primer    product size    genomic region 
-        
-        delimiters -- defaulted to tab
-        
-        output_file -- created only if the input given is a file opposed to a string. 
-                       If no name is specified for output_file, then it is automatically
-                       named matching_primers_output.txt
-        
-        Returns
-        -------------------------------------------------------------------------
-        matched_primers -- primer pair(s) that flank the given variant position(s).
-                           The distance of the variant from the forward and reverse 
-                           primer is given in bases/nucleotides. the tab deliminated 
-                           format outputted:
-                             variant_name   matched_primer_name distance_f  distance_r
-        
-        Examples
-        --------------------------------------------------------------------------
-        from primer_tools import primer_finder 
-        
-        primer_finder.matching_primer("15:48729400")
-        
-        primer_finder.matching_primer("variant_positions.txt,"matched_primers.txt")
-        
-        OR
-        
-        using click:
-        
-        python primer_finder --input_file 15:48729400 --primer_database primer_database.txt
+        \b\n
+        input can be either: FILE; tab deliminated file with variant name and variant
+        position, STRING;  variant position
+        \b\n
+        Examples:\b\n
+            primer_finder 15:48729400 --primer_database primer_database.txt
+            primer_finder --input_file in.txt --output_file out.txt 
         
     '''
     try:
         # get all genomic locations within primer pairs, from all primers in the database
         all_primer_pos = get_all_primer_pos(primer_database)
+    except IOError as e:
+        print primer_database+": "+e.strerror
+        sys.exit(0)
         
+    try:
         # if input is not an existing file and contains numbers and a colon 
         if os.path.isfile(input_file) is False and re.search(r"[0-9]:",input_file):             
             var_pos = re.sub(r'[^0-9:]','',input_file)      # remove any characters that isn't a number or colon
@@ -84,47 +44,34 @@ def matching_primer(input_file,
     
         # if input is a file 
         if os.path.isfile(input_file) is True:
-            with open(output_file,"w") as output_file:
+            var_file = open(input_file,"r+")
+            
+            all_matched_primers = []
+            
+            for var in var_file:
+                var_name = var.split("\t")[0]
+                var_pos = re.sub(r'[^0-9:]','',var.split("\t")[1])
+                matched_primers = match(var_pos,all_primer_pos,var_name)
+                print matched_primers
+                all_matched_primers.append(matched_primers)
+                
+            
+            if output_file is not None:
+                output_file = open(output_file,"w")
                 output_file.write("\t".join(("Variant_Name","Primer_Name",
                                             "distance_F(bp)","distance_R(bp)",
-                                            "\n"))
-                                )
-                var_file = open(input_file)
-                
-                for var in var_file:
-                    var_name = var.split("\t")[0]
-                    var_pos = re.sub(r'[^0-9:]','',var.split("\t")[1])
-                    matched_primers = match(var_pos,all_primer_pos,var_name)
-                    
-                    print matched_primers
-                    output_file.write("".join((matched_primers,"\n")))
-        
-        # give an error message
-        else:
-            return input_file + " is an invalid input"+"\n\n\t"+\
-                                "String or existing file can only be used as input."+"\n\t"+\
-                                "String must contain numbers and a colon."
-    except IOError as e:
-        return e.strerror+"\n"+primer_database+" does not exist."
-        
+                                            "\n")))
+                for matches in all_matched_primers:    
+                    output_file.write("".join((matches,"\n")))
+                output_file.close()                    
+
     except IndexError as e:
-        return e.args[0]+"\n\n"+ primer_database+" or "+input_file+" is incorrectly deliminated or an incorrect delimiter was specified."+"\n"    
-             
-                        
+        return e.args[0]+"\n\n"+input_file+\
+        " is incorrectly deliminated or an incorrect delimiter was specified."+"\n"    
             
 def get_all_primer_pos(primer_database):
     ''' Generate a list of every genomic position witin each primer pair given 
         in the primer database.
-    
-        Arguments
-        -----------------------------------------------------------------------
-        primer_database -- should be in the following format (tab deliminated):
-                              primer name    F-primer    R-primer    product size    genomic region 
-        
-        Returns
-        -----------------------------------------------------------------------
-        all_primers_pos_unpacked -- the aformentioned list in the following format:
-                                        primer_name genomic_position distance_postion_from_F_primer  distance_position_from_R_primer
     '''
     try:
         primer_file = open(primer_database,"r")
@@ -137,6 +84,7 @@ def get_all_primer_pos(primer_database):
         # generates all_primer_pos
         for i in primer_file:
             
+            # split into chromosome, start position and stop position in which primer occupies
             i = i.split("\t")
             primer_name = i[0]
             primer_range = re.split(r'[:-]',i[4])   # split into three components
@@ -144,7 +92,9 @@ def get_all_primer_pos(primer_database):
             start = int(primer_range[1])
             stop = int(primer_range[2])
             
-            
+            # use start and stop to iterate through every position in primer pair and
+            # output primer_name, primer positions, distance of primer position from the
+            # forward and reverse primer respectively and append to an empty list.
             all_pos_in_primer = [ " ".join((primer_name,chrom+":"+str(i),str(i-start),str(stop-i))) 
                                 for i in range(start,stop)]
             all_primer_pos.append(all_pos_in_primer)
@@ -154,27 +104,12 @@ def get_all_primer_pos(primer_database):
         return all_primers_pos_unpacked
         
     except IndexError:
-        raise
+        raise 
         
         
 def match(var_pos,primer_info,var_name=None):
     ''' Match a given variant position against every genomic position covered
         in all the primer pairs in the primer database.
-        
-        Arguments
-        -----------------------------------------------------------------------
-        var_pos -- variant position.
-        
-        primer_info -- the returned list from get_all_primer_pos() function.
-        
-        var_name -- the variant alias for the variant position. Defaulted to None.
-        
-        Returns
-        ------------------------------------------------------------------------
-        answer -- matched primers with distance of F/R primer from variant position.
-                  if no match is found then "No match found" is returned. 
-        
-        
     '''
     # used if a string given as input instead of a file
     if var_name is None:
--- primer_tools/unknown_primer.py
+++ primer_tools/unknown_primer.py
@@ -1,8 +1,7 @@
 from __future__ import division
 import requests,re, os, bs4, click
+from useful import gc_content
 
-
-# HG VERSION ALWAYS HG19 DESPITE WHAT YOU USE AS THE ARGUMENT FOR --HG_VERSION
 
 class AmbiguousBaseError(Exception):
     pass
@@ -16,12 +15,13 @@ class MultipleAmplicons(Exception):
 class IncorrectVariant(Exception):
     pass
 
+    
 @click.command()
-@click.option('--primers',nargs=2)
-@click.option('--input_file',nargs=1) # -1 means unlimited number of arguments
-@click.option('--output_file',default=None,help="default: unknwon_primer_output.txt")
+@click.option('--primers',nargs=2,help="accepts 2 arguments only; forward primer reverse primer")
+@click.option('--input_file',nargs=1, help="file used as input")
+@click.option('--output_file',default=None,help="only available if the --input_file option is used")
 @click.option('--hg_version',default="hg19",help="human genome version. default: hg19")
-@click.option('--delimiters',default="\t",help="character used to split F- & R-Primer, STRING only")
+@click.option('--delimiters',default="\t",help="--inut_file option only. default: tab")
 
 def unknown_primer(primers=None, input_file=None,output_file=None,
                    hg_version="hg19",delimiters=","):
@@ -31,11 +31,12 @@ def unknown_primer(primers=None, input_file=None,output_file=None,
         \b\n
         A file or string can be used as input.
         STRING: --primers option with 2 arguments (primer seqs) given.
-        FILE: --input_file option deliminated file with the primer name, F-primer seq anad R-primer seq.
+        FILE: --input_file option deliminated file with the primer name, 
+        F-primer seq anad R-primer seq.
         \b\n                    
         Example:\b\n
-        python unknown_primer.py --input_file in.txt --output_file out.txt --hg_version hg38
-        python unknown_primer.py --primers CGATCGTTGC GCTAGTCGT 
+           unknown_primer --input_file in.txt --output_file out.txt --hg_version hg38\n
+           unknown_primer --primers TAACAGATTGATGATGCATG CCCATGAGTGGCTCCTAAA 
     '''
     
     # if input is two arguments   
@@ -47,32 +48,49 @@ def unknown_primer(primers=None, input_file=None,output_file=None,
     if os.path.isfile(input_file) is True:
         input_file = open(input_file,"r+")
         primer_info = process_primer_info(input_file,output_file,hg_version,delimiters)
-        
+       
+        # if the --output_file is used, write the primer_info to a file
         if output_file is not None:
             output = open(output_file,"w")
+            header = "\t".join(("primer_name","F-primer","R-primer","amplicon_size",
+                               "genomic_range","gc%","number_amplicons","\n"))
+            output.write(header)
+            
             for primers in primer_info:
-                output.write(primers)
+                output.write(primers+"\n")
             output.close()
             return output
         
         return primer_info
-        
+                
 
 def process_primer_info(input_file,output_file,hg_version,delimiters):
+    '''
+    iterate through each line of the input and parse them into get_unknown_primer_info
+    '''
+    # adds all scrapped data to a list, which is written to an output, only --ouput_file is selected
     primer_info = []
+    
     for primer in input_file:
         try:
-            primer = primer.rstrip("\n").split(delimiters)
+            # splits up data into primer name, forward primer and reverse primer
+            primer = primer.split(delimiters)
             primer_name = primer[0]
             f_primer = primer[1].upper()
             r_primer = primer[2].upper()
+            
+            # ensure input is bases only
             if re.search(r'[^ATCG]',f_primer) or re.search(r'[^ATCG]',r_primer):
                 raise AmbiguousBaseError
-            amplicon_info = get_unknown_primer_info(primer_name,f_primer,r_primer,hg_version)
+            
+                    
+            amplicon_info = get_unknown_primer_info(hg_version,primer_name,f_primer,r_primer)
     
             print amplicon_info
             primer_info.append(amplicon_info)
-
+        
+        except IndexError:
+            print str(primer)+" is improperly deliminated"
         except AmbiguousBaseError:
             print "Skipping, non-ATGC base found in: "+primer_name
         except NoAmplicon:
@@ -80,25 +98,22 @@ def process_primer_info(input_file,output_file,hg_version,delimiters):
         except MultipleAmplicons:
             print "The following primers generate more than one amplicon:"+primer_name
         
+    return primer_info
     
-def get_unknown_primer_info(primer_name="query",f_primer=None,r_primer=None,hg_version=None):
+def get_unknown_primer_info(hg_version, primer_name="query",f_primer=None,r_primer=None):
 	''' Generate an amplicon sequence from inputted primer sequences, which
 	    is further manipulated t derive inofrmation from the sequence.
-	'''	
-	
-        # ensure input is only bases
-        if re.search(r'[^ATCG]',f_primer)or re.search(r'[^ATCG]',r_primer):
-            raise AmbiguousBaseError("Primers must contain ATGC bases only")
-        
-            
-        # generate amplicon sequence
-        req = requests.get("http://genome.ucsc.edu/cgi-bin/hgPcr?hg_version="+hg_version+\
-                           "&wp_target=genome&wp_f="+f_primer+"&wp_r="+r_primer+\
-                           "&wp_size=4000&wp_perfect=15&wp_good=15&boolshad"+\
-                           ".wp_flipReverse=0")
+	'''	     
+        # generate amplicon sequence using isPCR tool
+        req = requests.get("https://genome.ucsc.edu/cgi-bin/hgPcr?hgsid=483751629_"+\
+                           "vuLjoO4UVF9h4vF4TEp9U8OQiFd7&org=Human&db="+hg_version+\
+                           "&wp_target=genome&wp_f="+f_primer+"&wp_r="+r_primer+"&Submit=submit"+\
+                           "&wp_size=4000&wp_perfect=15&wp_good=15&boolshad.wp_flipReverse=0")
         req.raise_for_status()                                # get the request error code if failed
         entire_url = bs4.BeautifulSoup(req.text,"html.parser")
         pre_elements = entire_url.select('pre')               # get all <pre> elements on webpage
+        
+        # if nothing between pre-elements, raise error
         if not pre_elements:
             raise NoAmplicon("No amplicon generated")
         isPCR = pre_elements[0].getText()                     # get text for first <pre> element
@@ -110,8 +125,7 @@ def get_unknown_primer_info(primer_name="query",f_primer=None,r_primer=None,hg_v
         split_header =  amplicon_header[1:].split(" ")
         region = split_header[0].replace("+","-").replace("chr","")
         amplicon_size = split_header[1]
-        gc_percent = round(((amplicon.count("G")+amplicon.count("C")+amplicon.count("c")+\
-                    amplicon.count("g"))/ len(amplicon)) * 100,2)
+        gc_percent = gc_content(amplicon)
         product_number = len(pre_elements)
         if product_number > 1:
             raise MultipleAmplicons        
@@ -119,7 +133,7 @@ def get_unknown_primer_info(primer_name="query",f_primer=None,r_primer=None,hg_v
         
         # return scraped information
         output = (primer_name,f_primer,r_primer,str(amplicon_size),
-                  region,str(gc_percent)+"%",str(product_number))
+                  region,str(gc_percent),"%",str(product_number))
         return "\t".join(output)
 
 
