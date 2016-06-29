7038937fbb25ed6bea219139721b84568526a770
david <dross78375@gmail.com>
2016-06-29 07:51:13 +0100

    trying to scrape exon info but grch37.rest.ensembl.org isnt working and it am getting problems with the rest

--- get_seq/get_seq.py
+++ get_seq/get_seq.py
@@ -1,5 +1,5 @@
 from __future__ import division, print_function
-import os, sys,re, click, requests, bs4
+import os, sys,re, click, requests, bs4, json
 from pyensembl import EnsemblRelease
 from useful_tools.transcription_translation import transcription, translation
 from useful_tools.output import write_to_output
@@ -128,6 +128,8 @@ def get_seq(seq_name, var_pos, reference, trans, sanger, ensembl):
             # get gene information for the variant position
             gene_info = ensembl.get_gene_info()
             gene_name, gene_id, gene_type, gene_range = gene_info
+            transcript = ensembl.get_canonical_transcript(gene_name)
+            print(get_exon_numbers(seq_name, transcript, var_pos, ensembl))
 
             
 
@@ -153,11 +155,40 @@ def get_seq(seq_name, var_pos, reference, trans, sanger, ensembl):
                   +var_pos+" in "+seq_name+"\n")    
         except ErrorUCSC:
             print(var_pos+" in "+seq_name+" is not recognised by UCSC"+"\n")
+
+
+def get_exon_numbers(name, transcript, pos, ensembl):
+
+    exon_dics = ensembl.request_ensembl(transcript) 
+    exon_region = ensembl.all_exon_regions(exon_dics,transcript)
+    exon_id = ensembl.get_exon_id(exon_region,pos) # filter for exon id in which variant is within
+    if not exon_id:
+        sorted_exon_regions = sorted(exon_region)
+        intron_region = ensembl.all_intron_regions(sorted_exon_regions)
+        intron_num = ensembl.intron_number(intron_region,pos)
+        
+        if intron_num is None:
+            return "\t".join((name,pos,transcript,"NO INTRON/EXON MATCHED"))
+        else:
+            last_exon = total_exons(exon_dics,transcript)
+            last_intron = int(last_exon)-1
+            return "\t".join((query,pos,transcript,
+                              str(intron_num)+"/"+str(last_intron),"-"))
+    else:
+        exon_num = exon_number(exon_dics,exon_id,transcript)    # use th exon_id to get exon number
+        last_exon = total_exons(exon_dics,transcript)           # get total exons of transcript
+        
+        
+        return "\t".join((variant_alias,pos,transcript,"-",
+                          str(exon_num)+"/"+str(last_exon)))
+
+   
+
       
 
 
 class ScrapeEnsembl():
-    ''' NOT WORKING
+    ''' Let me become a SUPEER CLASS!
     '''
     def __init__(self, query, hg_version):
         self.query = query
@@ -189,10 +220,9 @@ class ScrapeEnsembl():
             return(gene_info)
     
     
-    def get_canonical_transcript(gene_name):
+    def get_canonical_transcript(self, gene_name):
         ''' Determine and return the canonical transcript of the given gene
         '''
-        # transcripts, I think he first tramscript retrurned is  the canonical
         all_transcripts = self.hg.transcript_ids_of_gene_name(gene_name)
         all_transcript_details = [self.hg.transcript_by_id(x) for x in all_transcripts]
         protein_coding_transcripts = []
@@ -205,13 +235,134 @@ class ScrapeEnsembl():
             stop = re.split(r"[:-]", location)[2]
             size = int(stop) - int(start)
             if transcript_type == "protein_coding":
-                protein_coding_transcripts.append((size,transcript,transcript_type))
+                protein_coding_transcripts.append((size,transcript,transcript_type)) 
 
         # sort by size and return the largest protein coding transcript
         canonical_transcript = sorted(protein_coding_transcripts)[-1][1]
         return canonical_transcript
 
+    
+    def request_ensembl(self,transcript):
+        ''' Retrieve all expanded exon information associated with a given transcript ID
+            in JSON format
+        '''
+        try:
+            if self.hg_version == 75:
+                grch = "grch37."
+            elif self.hg_version > 80:
+                grch = "."
+            else:
+                print("incompatible human genome version")
+            
+            url = "".join(("http://rest.ensembl.org/overlap/id/",transcript,
+                           "?feature=exon;content-type=application/json;expand=1"))
+            req = requests.get(url)
+            req.raise_for_status()
+            exon_dics = req.json()
+            return exon_dics
+        except requests.exceptions.RequestException as e:
+            return e
 
+
+    def all_exon_regions(self, exon_dics, transcript):
+        '''Get every exon ID and its start and stop position which resides within the
+           given transcript
+        '''
+        exon_region = set()
+        for exon_dicts in exon_dics:
+            if exon_dicts.get("Parent") == transcript:
+                rank = exon_dicts.get("rank")
+                start = exon_dicts.get("start")
+                end = exon_dicts.get("end")
+                exon = exon_dicts.get("exon_id")
+                exon_start_end = (rank, exon, start, end)
+                exon_region.add(exon_start_end)
+
+        return exon_region
+       
+
+    def all_intron_regions(self,sorted_exon_regions):
+        ''' Get all intron numbers, start and stop positions
+        ''' 
+        intron_region = set()
+        
+        for i in range(0,len(sorted_exon_regions)):
+            for x in range(0,len(sorted_exon_regions)):
+                if x == i +1:
+                    intron_number = str(sorted_exon_regions[i][0])
+                    end_pos_previous_exon = str(sorted_exon_regions[i][3])
+                    start_pos_next_exon = str(sorted_exon_regions[x][2])
+                    intron_info = (int(intron_number),end_pos_previous_exon,\
+                    start_pos_next_exon,str(int(end_pos_previous_exon)-
+                                            int(start_pos_next_exon)))
+                    intron_region.add(intron_info)
+        
+        return intron_region
+
+
+    def intron_number(self,intron_region,pos):
+        ''' Returns the intron number in which the variant is within
+        '''
+        for intron in intron_region:
+            if int(intron[3]) > 0:
+             # no idea why the start and end pos are swapped, hence the need for if/else
+                for x in range(int(intron[2]),int(intron[1])):                  
+                    if x == int(pos.split(":")[1]):
+                        intron_num= intron[0]
+                        return intron_num
+            else:
+                for x in range(int(intron[1]),int(intron[2])):
+                    if x == int(pos.split(":")[1]):
+                        intron_num= intron[0]
+                        return intron_num  
+
+                            
+    def get_exon_id(self,exon_region,pos):  
+        ''' Get the exon id for which the variant position is within.
+        
+            Use the exon_region tuple to iterate between the start and stop 
+            positions of each exon within the transcript until one number 
+            matches the variant position number, then return the exon_id 
+            associated with that position.
+        '''  
+        exon_id = []      
+        for exons in exon_region:
+            for x in range(exons[2],exons[3]):
+                if x == int(pos.split(":")[1]):
+                    exon_id.append(exons[1])
+        
+        return exon_id
+        
+
+
+
+    def exon_number(self,exon_dics,exon_id,transcript):
+        ''' Returns the exon number in which the variant is within.
+        
+            use the exon_id to retrieve the matching dictionary and extract
+            the rank (exon number) from that dictionary.
+        '''
+        for y in exon_dics:
+            if y.get("exon_id") == exon_id[0] and y.get("Parent") == transcript:
+                exon_number = y.get("rank")
+       
+        return exon_number
+
+            
+        
+    def total_exons(self,exon_dics,transcript):
+        ''' Get the last exon number in the transcript
+        
+            List comprehension which extracts all exon numbers found in all the 
+            dictionarys with the transcript id. Extract and return the last element
+            as the total_exons in the generated list.
+        '''
+        all_exon_ranks = [i.get("rank") for i in exon_dics 
+                          if i.get("Parent") == transcript]
+        total_exons = all_exon_ranks[-1]
+        return total_exons
+
+   
     
         
         
