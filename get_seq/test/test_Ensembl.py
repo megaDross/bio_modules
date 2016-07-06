eb8a0753103a8f3086e1379849417bb64dc52630
U-ED\dross11 <dross11@MVM-IGMM-L025.med.ed.ac.uk>
2016-07-06 17:16:36 +0100

    All testing written thus far has been successfully converted to unittesting from pytest

--- get_seq/test/test_Ensembl.py
+++ get_seq/test/test_Ensembl.py
@@ -1,10 +1,17 @@
-# python3 -m pytest test_Ensembl.py
 from get_seq.Ensembl import *
-# this doesnt work, try with unnittesting
+import unittest
+# python3 -m unittest test_Ensembl.py
 
-def check_gene_info():
-    assert ScrapeEnsembl("15:48762884", "hg19").get_gene_info() == (
-        'FBN1', 'ENSG00000166147', 'protein_coding', '15:48700503-48938046')
-    assert ScrapeEnsembl('16:15812194', "hg19").get_gene_info() == (
-        'MYH11', 'ENSG00000133392', 'protein_coding', '16:15797029-15950890')
-    
+
+class TestGeneTranscript(unittest.TestCase):
+    def test_gene_info(self):
+        self.assertEqual(ScrapeEnsembl("15:48762884", "hg19").get_gene_info(),(
+            'FBN1', 'ENSG00000166147', 'protein_coding', '15:48700503-48938046'))
+        self.assertEqual(ScrapeEnsembl('16:15812194', "hg19").get_gene_info(),(
+            'MYH11', 'ENSG00000133392', 'protein_coding', '16:15797029-15950890'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+        
