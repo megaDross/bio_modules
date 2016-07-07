48948d8762c0044f5abfb32254ed8b8d73d09f1d
david <dross78375@gmail.com>
2016-07-07 07:53:13 +0100

    get_gene_info() test for hg38

--- get_seq/test/test_Ensembl.py
+++ get_seq/test/test_Ensembl.py
@@ -4,11 +4,17 @@ import unittest
 
 
 class TestGeneTranscript(unittest.TestCase):
-    def test_gene_info(self):
+    def test_gene_info_hg19(self):
         self.assertEqual(ScrapeEnsembl("15:48762884", "hg19").get_gene_info(),(
             'FBN1', 'ENSG00000166147', 'protein_coding', '15:48700503-48938046'))
         self.assertEqual(ScrapeEnsembl('16:15812194', "hg19").get_gene_info(),(
             'MYH11', 'ENSG00000133392', 'protein_coding', '16:15797029-15950890'))
+    def test_gene_info_hg38(self):        
+        self.assertEqual(ScrapeEnsembl("15:48762884", "hg38").get_gene_info(),(
+            'CEP152', 'ENSG00000103995', 'protein_coding', '15:48712928-48811146'))
+        self.assertEqual(ScrapeEnsembl("16:15812194", "hg38").get_gene_info(),(
+            'MYH11', 'ENSG00000133392', 'protein_coding', '16:15703135-15857033'))
+
 
 
 if __name__ == '__main__':
