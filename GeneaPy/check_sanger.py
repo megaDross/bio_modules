80fba0288a01d82036da72c6fe12dbaa828f25cb
david <dross78375@gmail.com>
2016-08-28 12:49:21 +0100

    temp

--- GeneaPy/check_sanger.py
+++ GeneaPy/check_sanger.py
@@ -255,7 +255,7 @@ class CompareSeqs(object):
         for count, index in zip(counts, indexes_sequence):
             if sequence[count] in het_call_dict.get(index):
                 matched_seq.append(het_call_dict[index])
-        print(counts)        
+        print(len(matched_seq))
         return matched_seq 
 
 
