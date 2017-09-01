da262880d85d5868003ec0204b00ab728689f2d3
superDross <dross78375@gmail.com>
2017-09-01 15:55:57 +0100

    cleaned up scraper

--- GeneaPy/Ensembl.py
+++ GeneaPy/Ensembl.py
@@ -2,7 +2,7 @@ import re, ensembl_exon
 from pyensembl import EnsemblRelease
 
 
-class GeneInfo(object):
+class GeneMetaData(object):
     ''' Store gene, transcript and exon metadata of a given genomic position.
     
     Attributes:
@@ -14,10 +14,11 @@ class GeneInfo(object):
     def __init__(self, query, hg_version):
         self.query = query.replace("chr","")
         self.hg_version = hg_version 
-        self.hg = EnsemblRelease(GeneInfo.genome.get(hg_version)) # convert to ensembl release object
+        self.hg = EnsemblRelease(self.genome.get(hg_version)) # convert to ensembl release object
         self.gene, self.id, self.type, self.location = self.get_gene_info()
         self.transcript = self.get_canonical_transcript()
         self.exon_id, self.intron, self.exon = self.get_exon_info()
+
 
     def get_gene_info(self):
         ''' Get the gene information at a given genomic position.
@@ -36,26 +37,20 @@ class GeneInfo(object):
                 return(gene_info)
 
         except IndexError:
-                print(" ".join(("No gene found at",self.query,"for genome version", str(self.hg_version))))
+                print("No gene found at {} for genome version {}".format(self.query, str(self.hg_version)))
 
 
     def get_canonical_transcript(self):
         ''' Determine and return the canonical transcript of the given gene.
         '''
         all_transcripts = self.hg.transcript_ids_of_gene_name(self.gene)
-        all_transcript_details = [self.hg.transcript_by_id(x) for x in all_transcripts]
+        all_transcripts = [self.hg.transcript_by_id(x) for x in all_transcripts]
         protein_coding_transcripts = []
-        for x in all_transcript_details:
-            split_transcript_info = re.split(r"[=,]",str(x))
-            transcript = split_transcript_info[1]
-            transcript_type = split_transcript_info[9]
-            location = split_transcript_info[-1][:-1]
-            start = re.split(r"[:-]", location)[1]
-            stop = re.split(r"[:-]", location)[2]
-            size = int(stop) - int(start)
-            if transcript_type == "protein_coding":
-                protein_coding_transcripts.append((size,transcript,transcript_type)) 
-        
+
+        for transcript in all_transcripts:
+            size = transcript.end - transcript.start
+            if transcript.biotype == "protein_coding":
+                protein_coding_transcripts.append((size, transcript.id, transcript.biotype)) 
         # sort by size and return the largest protein coding transcript
         if protein_coding_transcripts:    
             canonical_transcript = sorted(protein_coding_transcripts)[-1][1]
@@ -63,14 +58,18 @@ class GeneInfo(object):
 
 
     def get_exon_info(self):
-        ''' From a transcript and genomic position, determine the exon number
-            using the ExonInfo class within the Ensmebl module
+        ''' Get the exon/intron numbers and IDs.
         '''
         return ensembl_exon.main(self.transcript, self.hg_version, self.query)    
 
 
+    def update_transcript(self, transcript):
+        ''' Update transcript ID and Exon info.
 
-
+        NOTE: returns No Intron/Exon Matched error. 
+        '''
+        self.transcript = transcript
+        self.exon_id, self.intron, self.exon = self.get_exon_info()
 
 
 
